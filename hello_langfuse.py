77827eb64d6f14cd0ed494783c73ab29ca2d974c
Luis Zermeno <lgzg90@hotmail.com>
2026-04-22 18:15:56 -0600

    multi trace loop tried and working

--- hello_langfuse.py
+++ hello_langfuse.py
@@ -6,19 +6,33 @@ from langfuse.langchain import CallbackHandler
 
 load_dotenv()
 
+#This loads the env langfuse elements and creates the opentelemetry processor
+#Handler makes the langfuse call.
 langfuse_handler = CallbackHandler()
 
+#Call openrouter using the Openai SDK
 model = ChatOpenAI(
     model="nvidia/nemotron-3-super-120b-a12b:free",
     base_url="https://openrouter.ai/api/v1",
     api_key=os.getenv("OPENROUTER_API_KEY"),
 )
 
+#Wrapper, will create extra layers internally
 agent = create_agent(model=model)
 
-response = agent.invoke(
-    {"messages": [{"role": "user", "content": "In one sentence, what is LLM observability?"}]},
-    config={"callbacks": [langfuse_handler]},
-)
 
-print(response)
+while True:
+
+    question = input("\nHere your question:\n")
+
+    if question == "exit":
+        break
+
+    else:
+
+        response = agent.invoke(
+            {"messages": [{"role": "user", "content": question}]},
+            config={"callbacks": [langfuse_handler]},
+        )
+
+        print(response)
